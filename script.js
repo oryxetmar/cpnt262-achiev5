1759bbdfcf35e4d6efb2d56c3f3a010c42b07d39
oryxetmar <tori.taylor@edu.sait.ca>
2022-02-21 14:38:38 -0700

    readme

---
+++ script.js
@@ -0,0 +1,11 @@
+const marvelChar = ["Loki", "Jean Grey", "Storm", "Rogue", "Wasp", "Jessica Jones","Squirrel Girl"]
+
+let output = 2;
+
+for (let i = 0; i <= marvelChar.length; i++) {
+   
+    console.log(marvelChar[i]);
+}
+
+document.getElementById("demo").innerHTML=marvelChar[5];
+
